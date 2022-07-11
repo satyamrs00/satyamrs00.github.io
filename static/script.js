bfc23eaa3639728d28a606887d23fbf0dcd63ca9
Satyam Sagar <satyamsagar1711@gmail.com>
2022-07-12 04:16:28 +0530

    added project 1

--- static/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let height = screen.height;
-    console.log(height);
-    document.getElementById('firstGif').style.height = `${height}px`;
-})
