cd4ee52bc7e4f791591ffc3229beffe717d89339
Satyam Sagar <satyamsagar1711@gmail.com>
2022-07-12 03:13:43 +0530

    home section created

---
+++ static/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let height = screen.height;
+    console.log(height);
+    document.getElementById('firstGif').style.height = `${height}px`;
+})
