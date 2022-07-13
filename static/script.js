78b4113f5b6574d5e06be47d4b1e3eff12bb6273
Satyam Sagar <satyamsagar1711@gmail.com>
2022-07-14 00:16:47 +0530

    added about page

--- static/script.js
+++ static/script.js
@@ -17,4 +17,25 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('eplbtn').onclick = () => document.getElementById('explore').scrollIntoView({behavior: 'smooth'});
     document.getElementById('navexplore').onclick = () => document.getElementById('explore').scrollIntoView({behavior: 'smooth'});
     document.getElementById('navcontact').onclick = () => document.getElementById('contact').scrollIntoView({behavior: 'smooth'});
+
+    document.getElementById('navabout').onclick = () => {
+        document.getElementById('about').style.display = "block";
+        document.getElementById('about').scrollIntoView({behavior: 'smooth'});
+    }
+    document.getElementById('abouts1').onclick = () => {
+        document.getElementById('about').style.display = "none";
+    }
+    const spans = document.querySelectorAll('#aboutd4 span');
+
+    [...spans].forEach(span => {
+        span.addEventListener('mouseover', () => {
+            span.children.item(1).innerHTML = span.dataset.name;
+        });
+    });
+    [...spans].forEach(span => {
+        span.addEventListener('mouseout', () => {
+            span.children.item(1).innerHTML = "";
+        });
+    })
+    
 });
