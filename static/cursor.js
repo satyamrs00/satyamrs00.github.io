be85780c1a1387d6ed7fddd4e01e253a2076b222
Satyam Sagar <satyamsagar1711@gmail.com>
2022-09-28 23:39:48 +0530

    implemented new cursor

---
+++ static/cursor.js
@@ -0,0 +1,114 @@
+const App = {
+	cursorEl: document.getElementById("cursor"),
+
+	linkEls: document.querySelectorAll(".cursorhover, .cursorhide"),
+    hideEls: document.querySelectorAll(".cursorhide"),
+	currentlyHoveredLinkEl: undefined,
+
+	lastClearTransitionTimeout: undefined,
+
+	init() {
+		this.handleMouseMove()
+		this.handleLinksHover()
+	},
+
+	handleMouseMove() {
+		const onMouseMove = (ev) => {
+			const { clientX, clientY } = ev
+
+			const CIRCULAR_POINTER_SIZE = 30
+
+			let newCursorX = undefined
+			let newCursorY = undefined
+
+			let newCursorWidth = undefined
+			let newCursorHeight = undefined
+
+			let newCursorBorderRadius = undefined
+            let newCursorOpacity = undefined
+
+            let newCursorBackground = undefined
+
+            if (this.currentlyHoveredLinkEl) {
+				const currentlyHoveredLinkBoundings =
+					this.currentlyHoveredLinkEl.getBoundingClientRect()
+
+                if(this.currentlyHoveredLinkEl.classList.contains("cursorhide")) {
+                    newCursorOpacity = 0
+                } else {
+                    newCursorOpacity = 1
+                }
+
+				newCursorX = currentlyHoveredLinkBoundings.x + 15
+				newCursorY = currentlyHoveredLinkBoundings.y + 15
+
+				newCursorWidth = currentlyHoveredLinkBoundings.width
+				newCursorHeight = currentlyHoveredLinkBoundings.height
+
+				newCursorBorderRadius = window.getComputedStyle(this.currentlyHoveredLinkEl).borderRadius || 0
+
+                newCursorBackground = "#CD32C4"
+
+                
+			} else {
+
+				newCursorX = clientX
+				newCursorY = clientY
+
+				newCursorWidth = CIRCULAR_POINTER_SIZE
+				newCursorHeight = CIRCULAR_POINTER_SIZE
+
+				newCursorBorderRadius = "0%"
+
+                newCursorOpacity = 1
+
+                newCursorBackground = "radial-gradient(circle closest-side, #CD32C4, transparent)"
+			}
+
+			if (newCursorWidth + "px" !== this.cursorEl.style.width) {
+				this.cursorEl.style.transition = "0.110s all"
+
+				clearTimeout(this.lastClearTransitionTimeout)
+				this.lastClearTransitionTimeout = undefined
+			} else {
+				if (!this.lastClearTransitionTimeout) {
+					this.lastClearTransitionTimeout = setTimeout(() => {
+						this.cursorEl.style.transition = "none"
+
+						this.lastClearTransitionTimeout = undefined
+					}, 110)
+				}
+			}
+
+			this.cursorEl.style.opacity = newCursorOpacity
+			this.cursorEl.style.transform = `translate(
+				${newCursorX}px,
+				${newCursorY}px
+			)`
+			this.cursorEl.style.width = newCursorWidth + "px"
+			this.cursorEl.style.height = newCursorHeight + "px"
+			this.cursorEl.style.borderRadius = newCursorBorderRadius
+            this.cursorEl.style.background = newCursorBackground
+		}
+
+		window.addEventListener("mousemove", (ev) => {
+			requestAnimationFrame(() => onMouseMove(ev))
+		})
+	},
+
+	handleLinksHover() {
+		this.linkEls.forEach((linkEl) => {
+			linkEl.onmouseover = () => {
+				this.currentlyHoveredLinkEl = linkEl
+			}
+
+			linkEl.onmouseout = () => {
+				if (this.currentlyHoveredLinkEl == linkEl) {
+					this.currentlyHoveredLinkEl = undefined
+				}
+			}
+		})
+	}
+}
+
+App.init()
